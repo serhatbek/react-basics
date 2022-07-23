a8174e8f4610db3e8b87199df983bc521b784ddf
serhatbek <serhat.bekk@gmail.com>
2022-07-23 23:18:53 +0300

    Counter App, useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import './App.css';
-
-const Person = ({ name, lastName, age }) => {
-  return (
-    <div className='person'>
-      <h1>Name: {name}</h1>
-      <h2>Last Name: {lastName}</h2>
-      <h2>Age: {age}</h2>
-    </div>
-  );
-};
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('current count is ' + count);
+  }, [count]);
+
+  const incCounter = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
+
+  const decCounter = () => {
+    setCount((prevCount) => {
+      return prevCount - 1;
+    });
+  };
+
   return (
     <div className='App'>
-      <h1>Hello {2 + 2}!</h1>
-      <Person name='Toprak' lastName={'Bek'} age={28} />
-      <Person name='Züleyha' lastName={'Karakaş'} age={38} />
-      <Person name='Melike' lastName={'Karakaş'} age={14} />
-      <Person name='Bahar' lastName={'Karakaş'} age={8} />
+      <div>
+        <h2>{count}</h2>
+        <button onClick={incCounter}>+</button>
+        <button onClick={decCounter}>-</button>
+      </div>
     </div>
   );
 };
