e651db711bb90068e644c1e2226f12ea55923bac
serhatbek <serhat.bekk@gmail.com>
2022-07-23 22:59:09 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
-function App() {
+const Person = ({ name, lastName, age }) => {
   return (
-    <div>
-      <h1>Meyeww</h1>
+    <div className='person'>
+      <h1>Name: {name}</h1>
+      <h2>Last Name: {lastName}</h2>
+      <h2>Age: {age}</h2>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello {2 + 2}!</h1>
+      <Person name='Toprak' lastName={'Bek'} age={28} />
+      <Person name='Züleyha' lastName={'Karakaş'} age={38} />
+      <Person name='Melike' lastName={'Karakaş'} age={14} />
+      <Person name='Bahar' lastName={'Karakaş'} age={8} />
+    </div>
+  );
+};
 
 export default App;
